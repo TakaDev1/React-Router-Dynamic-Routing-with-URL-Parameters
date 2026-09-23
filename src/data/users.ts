d67d54221521cb554adf8a6f143edb8fa419788f
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-23 18:02:05 +0900

    feat: Implement users data

---
+++ src/data/users.ts
@@ -0,0 +1,14 @@
+import type { User } from "../types/user";
+
+const users: Record<string, User> = {
+  "1": {
+    name: "Alice",
+    email: "alice.example.com",
+  },
+  "2": {
+    name: "Bob",
+    email: "bob.example.com",
+  },
+};
+
+export default users;
