e74a6c4ff1b3640c69f74b4dbb6edb3314a47f75
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-23 18:16:17 +0900

    feat: Implement UserProfile Page

---
+++ src/pages/UserProfile.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useParams } from "react-router";
+import users from "../data/users";
+
+const UserProfile = () => {
+  const { id } = useParams();
+
+  const user = id ? users[id] : undefined;
+  return (
+    <div>
+      {user ? (
+        <div>
+          <p>名前: {user.name}</p>
+          <p>メールアドレス: {user.email}</p>
+        </div>
+      ) : (
+        <p>User Not Found</p>
+      )}
+    </div>
+  );
+};
+
+export default UserProfile;
